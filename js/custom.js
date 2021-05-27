b72ba9a9e61b08cd211a65d462bd958c9defc60a
skynetfrank <frank.uah@gmail.com>
2021-05-26 20:14:31 -0400

    commiting now

--- js/custom.js
+++ js/custom.js
@@ -41,12 +41,11 @@ const formLogin = document.getElementById("appointment-form");
 const usuarioActual = document.getElementById("usuario");
 
 (async function inicio() {
-  await configDia();
-
   //solo se puede apartar citas a partir del dia actual (dias pasados son inhabilitados)
   const fechaDeHoy = new Date(); // Gets today's date
   let minDate = convertirFecha(fechaDeHoy);
   document.getElementById("fecha").setAttribute('min', minDate);
+  document.getElementById("div-hora").style.display = "none";
 
 })();
 
@@ -71,63 +70,58 @@ function arrayRemove(arr, value) {
 }
 
 
-
 //inicio de sesion
 formLogin.addEventListener('submit', async (e) => {
-
   e.preventDefault();
-
-  const email = formLogin["email"].value;
-  const pw = formLogin["password"].value;
-  const nombre = formLogin["nombre"].value;
-  const apellido = formLogin["apellido"].value;
-  const cedula = formLogin["cedula"].value;
+  let email = formLogin["email"].value;
+  let pw = formLogin["password"].value;
+  let nombre = formLogin["nombre"].value;
+  let apellido = formLogin["apellido"].value;
+  let cedula = formLogin["cedula"].value;
   const hora = formLogin["hora"].value;
   const fecha = formLogin["fecha"].value;
   const mensaje = formLogin["mensaje"].value;
 
-
   try {
     //Log-in con email y password
     if (!nombre && !apellido && !cedula && !hora && !fecha) {
-      console.log("Es autenticacion")
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        .then(() => {
-          // Existing and future Auth states are now persisted in the current session only.
-          // Closing the window would clear any existing state even if a user forgets to sign out.
-          return firebase.auth().signInWithEmailAndPassword(email, pw);
-
-        }).catch((error) => alert(error.message));
+      console.log("Log-in inicio de sesion con email y password")
+      await auth.signInWithEmailAndPassword(email, pw);
     }
 
     // sign-up con datos adicionales
     if (nombre && apellido && cedula) {
-      console.log("Sig-Up Registro de nuevo usuario")
-      const credencial = await auth.createUserWithEmailAndPassword(email, pw);
-      db
-        .collection("users")
-        .doc(credencial.user.uid)
-        .set({
-          nombre: nombre,
-          apellido: apellido,
-          cedula: cedula,
-        });
+
+      const prueba = document.getElementById("email").value;
+
+      auth.createUserWithEmailAndPassword(email, pw)
+        .then((result) => {
+          console.log("el nuevo usuario uid es: ", result.user.uid);
+          db
+            .collection("users")
+            .doc(result.user.uid)
+            .set({
+              nombre: nombre,
+              apellido: apellido,
+              cedula: cedula,
+            });
+          document.getElementById("email").value = ''
+          console.log("Promesa resuelta hacia el cambio de auth");
+          /* document.getElementById("nombre").value = '';
+          document.getElementById("apellido").value = '';
+          document.getElementById("cedula").value = '';
+          document.getElementById("email").value = ''
+          document.getElementById("password").value = ''; */
+        })
+        .catch((error) => console.log("error al registrar usuario nuevo=====>>>: ", error));
+
+
+
     }
+
 
     //Agendar una cita -firebase-
     if (fecha && hora) {
-      db.collection("citas")
-        .where('fecha', '==', fecha)
-        .get()
-        .then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log("cita apartada el:", " ====> ", doc.data().fecha, doc.data().hora);
-          });
-        })
-        .catch((error) => {
-          console.log("Error getting documents: ", error);
-        });
 
       db
         .collection("citas")
@@ -138,17 +132,30 @@ formLogin.addEventListener('submit', async (e) => {
           msg: mensaje,
           paciente: auth.currentUser.uid,
         });
+      console.log("Se a Apartado una cita OK el dia: ", fecha, " a las: ", hora);
       document.getElementById("fecha").value = '';
       document.getElementById("hora").value = '';
-
+      document.getElementById("div-hora").style.display = "none"
 
     }
+
+
   } catch (error) {
     if (error.code === "auth/email-already-in-use") {
-      alert("Este Email ya ha sido registrado... verifique!");
+      alert("Este Email ya ha sido registrado. Verifique!");
     }
+    if (error.code === "auth/invalid-email") {
+      alert("El email esta mal escrito o el campo esta vacio. Verifique!");
+    }
+    if (error.code === "auth/user-not-found") {
+      alert("Este usuario no existe. Registrate en el link de abajo para iniciar sesion.");
+    }
+    if (error.code === "auth/wrong-password") {
+      alert("Disculpe, introdujo una clave erronea. Verifique.");
+    }
+
   }
-})//eo inicio de sesion 
+})//fin de inicio de sesion 
 
 
 
@@ -164,54 +171,29 @@ async function configDia() {
   }
 
   //tabla de horas por defecto de 7 a 7 (24 horas)
-  var horas = ['07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30',
-    '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30',
-    '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', ''];
+  var horas = ['08:00', '09:00', '10:00', '11:00', '13:00', '14:00', '15:00', '16:00', ''];
 
   await db.collection("citas")
     .where('fecha', '==', fecha)
     .onSnapshot((querySnapshot) => {
-
+      //este consulta devuelve las horas apartadas por usuario en la fecha del where
+      //si devuelve algun dato (querysnapshot) se ejecuta este bloque forEach sino lo ignora 
       querySnapshot.forEach((doc) => {
-        console.log("querysnapshot ForEach:", doc.data().hora);
+        //esta hora (doc.data().hora) se elimina del rango de horas ya que esta apartada por usuario
+        console.log("La Hora: ", doc.data().hora, " se elimino del rango de Horas para el control")
         let position = horas.indexOf(doc.data().hora);
-        console.log("position:", position);
         horas.splice(position, 1)
-        console.log("horas despues del slice dentro del foreach", horas);
-
       });
-      console.log("Horas after snapshot: ", horas);
+      //El array con el rango de horas ha sido -cleaned- solo quedan las horas no apartadas
+      //para este dia especifico del where de la consulta     
       const tablaHoras = document.getElementById("datalist-horas");
       let options = '';
       for (let i = 0; i < horas.length; i++) {
         options += '<option value="' + horas[i] + '" />';
-
       }
       tablaHoras.innerHTML = options;
-
     });
 
-
-  /* await db.collection("citas")
-    .where('fecha', '==', fecha)
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        let position = horas.indexOf(doc.data().hora);
-        horas.splice(position, 1);
-      });
-    })
-    .catch((error) => {
-      console.log("Error getting documents: ", error);
-    });
- */
-
-
-
-
-
-  return "done";
 }
 
 
@@ -226,6 +208,7 @@ async function configDia() {
 
 
 inputFecha.addEventListener('change', async (e) => {
+  document.getElementById("div-hora").style.display = "inline-block"
   await configDia();
   const fechaSeleccionada = new Date(e.target.value);
   if (fechaSeleccionada.getDay() == 6 || fechaSeleccionada.getDay() == 5) {
@@ -240,7 +223,11 @@ inputFecha.addEventListener('change', async (e) => {
 document.getElementById("myTable").addEventListener('click', (e) => {
   e.preventDefault();
   const id_cita = e.target.parentNode.parentNode.querySelector('td:nth-child(1)').innerHTML;
-  db.collection("citas").doc(id_cita).delete().then(() => console.log(result)).catch(error => console.log("error al eliminar cita! verifique..."));
+  db.collection("citas").doc(id_cita)
+    .delete()
+    .then()
+    .catch(error => console.log("error al eliminar cita! verifique..."));
+  configDia();
 });
 
 
@@ -255,10 +242,11 @@ auth.onAuthStateChanged((user) => {
 
 
   if (user) {
-    console.log("auth changed: hay user");
+    console.log("AUTH CHANGED: USUARIO LOGEADO OK SETTING UX");
 
     //set up UI para usuarios autenticados sin novedad.
     usuarioActual.innerHTML = 'hola: ' + user.email;
+    document.getElementById("link-registrar").style.display = "none";
     document.getElementById("link-logout").style.display = "inline-block";
     document.getElementById("div-email").style.display = "none";
     document.getElementById("div-password").style.display = "none";
@@ -266,12 +254,10 @@ auth.onAuthStateChanged((user) => {
     document.getElementById("div-apellido").style.display = "none";
     document.getElementById("div-cedula").style.display = "none";
     document.getElementById("div-fecha").style.display = "inline-block";
-    document.getElementById("div-hora").style.display = "inline-block";
+    //document.getElementById("div-hora").style.display = "inline-block";
     document.getElementById("fecha").setAttribute('required', 'true');
     document.getElementById("hora").setAttribute('required', 'true');
     document.getElementById("div-mensaje").style.display = "inline-block";
-  
-    document.getElementById("link-registrar").style.display = "none";
     document.getElementById("cf-submit").innerHTML = "Apartar Cita";
 
 
@@ -296,20 +282,20 @@ auth.onAuthStateChanged((user) => {
                       </tr>`;
             table.innerHTML += row
           })
-          if(table.innerHTML===''){
+          if (table.innerHTML === '') {
             document.getElementById("mostrar-citas").style.display = "none";
           }
-          
+
         });
     }
     citasPendientes();
-    
-   
+
+
 
     //fin de set up UI para usuarios autenticados sin novedad. 
 
   } else {
-    console.log("No hay usuario: establecer UX para login");
+    console.log("AUTH CHANGED! : No hay usuario establecer UX para login");
     document.getElementById("fecha").value = '';
     document.getElementById("hora").value = '';
     document.getElementById("fecha").removeAttribute('required');
@@ -323,7 +309,7 @@ auth.onAuthStateChanged((user) => {
     document.getElementById("cf-submit").innerHTML = "Iniciar Sesion";
     document.getElementById("mostrar-citas").style.display = "none";
     document.getElementById("link-registrar").style.display = "inline-block";
-    console.log("Set UX para login porque no hay usuario DONE!")
+
   }
 })
 
@@ -331,6 +317,7 @@ auth.onAuthStateChanged((user) => {
 //set up UI cuando se va a crear una cuenta nueva
 document.getElementById("link-registrar").addEventListener('click', () => {
   console.log("clicked en el link registrar");
+  document.getElementById("link-registrar").style.display = "none";
   document.getElementById("div-email").style.display = "inline-block";
   document.getElementById("div-password").style.display = "inline-block";
   document.getElementById("email").setAttribute('value', '');
@@ -338,17 +325,15 @@ document.getElementById("link-registrar").addEventListener('click', () => {
   document.getElementById("div-nombre").style.display = "inline-block";
   document.getElementById("div-apellido").style.display = "inline-block";
   document.getElementById("div-cedula").style.display = "inline-block";
-  document.getElementById("nombre").setAttribute('value', '');
-  document.getElementById("apellido").setAttribute('value', '');
-  document.getElementById("cedula").setAttribute('value', '');
+  document.getElementById("nombre").value = '';
+  document.getElementById("apellido").value = '';
+  document.getElementById("cedula").value = '';
 
-
-
-  document.getElementById("cf-submit").innerHTML = "registrar";
+  document.getElementById("cf-submit").innerHTML = "Registrarse";
   document.getElementById("nombre").setAttribute('required', true);
   document.getElementById("apellido").setAttribute('required', true);
   document.getElementById("cedula").setAttribute('required', true);
-  linkRegistrar.style.display = "none";
+
 })
 
 //ojo con este codigo hay que evitar que el usuario introduzca una fecha
@@ -372,4 +357,15 @@ document.getElementById("link-logout").addEventListener("click", () => {
   document.getElementById("div-password").style.display = "inline-block"
   document.getElementById("email").value = '';
   document.getElementById("password").value = '';
+})
+
+
+inputHora.addEventListener('click', (e) => {
+
+  console.log("Input-hora evento click disparado")
+})
+
+inputHora.addEventListener('focus', (e) => {
+
+  console.log("Input-hora evento focus disparado")
 })
